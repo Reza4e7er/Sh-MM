24d14a4521b3e37156ea10e57d40280daea385b2
Mehdi-Saleh <saleh.mehdi1381@gmail.com>
2022-08-18 16:44:33 +0430

    Heyyy

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -6,29 +6,29 @@ public class InputManager : MonoBehaviour
 {
     public DirectionState directionState = DirectionState.Forward;
 
-    private void Update()
-    {
-        directionState = DirectionState.Forward;
+    // private void Update()
+    // {
+    //     directionState = DirectionState.Forward;
 
-        if (Input.touchCount>0)
-        {
-            Touch touch = Input.GetTouch(0);
+    //     if (Input.touchCount>0)
+    //     {
+    //         Touch touch = Input.GetTouch(0);
 
-            // check if touch should be acounted
-            // not implemented!
+    //         // check if touch should be acounted
+    //         // not implemented!
 
 
-            // turn
-            if (touch.position.x>=Screen.currentResolution.width/2)
-            {
-                directionState = DirectionState.Right;
-            }
-            else
-            {
-                directionState = DirectionState.Left;
-            }
-        }
-    }
+    //         // turn
+    //         if (touch.position.x>=Screen.currentResolution.width/2)
+    //         {
+    //             directionState = DirectionState.Right;
+    //         }
+    //         else
+    //         {
+    //             directionState = DirectionState.Left;
+    //         }
+    //     }
+    // }
 }
 
 public enum DirectionState
