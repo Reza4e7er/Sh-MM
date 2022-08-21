d46b1f59a60c07e544cee37a0d30de7107c40495
Mehdi-Saleh <saleh.mehdi1381@gmail.com>
2022-08-21 15:25:38 +0430

    a

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     [HideInInspector] public Vector2 moveDirection = Vector2.up;
+    public bool isPlayer = false;
     public float baseMoveSpeed = 1f;
     public float currentMoveSpeed;
-    // public GameObject glassesMeshObject;
+    [SerializeField] private float maxHealth = 10f;
+    [SerializeField] private float health;
     private Rigidbody _rigidbody;
     public Animator animator;
 
@@ -17,6 +19,7 @@ public class Character : MonoBehaviour
         animator = GetComponentInChildren<Animator>();
 
         currentMoveSpeed = baseMoveSpeed;
+        health = maxHealth;
     }
 
     private void Update()
@@ -25,5 +28,19 @@ public class Character : MonoBehaviour
         _rigidbody.MovePosition(transform.position+new Vector3(moveDirection.x, 0f, moveDirection.y)*Time.deltaTime*currentMoveSpeed);
         // rotate the character
         _rigidbody.MoveRotation(Quaternion.Euler(0f, Mathf.Atan2(moveDirection.x, moveDirection.y)*Mathf.Rad2Deg, 0f));
+    }
+
+    // called by the bullets to damage this character
+    public void ApplyDamage(float damageAmount)
+    {
+        health -= damageAmount;
+        if (health<=0f)
+            Die();
+    }
+
+    // called when the character dies
+    public void Die()
+    {
+        gameObject.SetActive(false);
     }
 }
