419fa27ac053b7d7184fb83c8d0f1c1ef73c2f30
Mehdi-Saleh <saleh.mehdi1381@gmail.com>
2022-09-05 16:50:38 +0430

    added calculation groups support

--- Assets/Scripts/Controllers/EnemyController.cs
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -8,20 +8,34 @@ public class EnemyController : MonoBehaviour
     //[SerializeField] private float maxDistanceToPlayerToAccack = 1f;
     public List<Character> characters;
 
+    public int groupsCount = 3;
+    private int groupsI = 0;
+
 
     // calculates directions for all of the characters (called by the MainController)
     public void Calculate()
     {
+        CalculateGroup(groupsCount, groupsI);
+
+        if (groupsI>=groupsCount)
+            groupsI = 0;
+        else
+            groupsI++;
+    }
+
+    // calculates directions for a portion of the characters
+    public void CalculateGroup(int groupsCount, int i)
+    {
         Vector2 newDir = new Vector2();
 
-        foreach (Character character in characters)
+        for (int j=i; j<characters.Count; j+=groupsCount)
         {
-            newDir.x = target.position.x - character.transform.position.x;
-            newDir.y = target.position.z - character.transform.position.z;
-            if (!character.isPlayer && !character.isAttacking && newDir.magnitude<=character.attackRange)
-                character.Attack();
+            newDir.x = target.position.x - characters[j].transform.position.x;
+            newDir.y = target.position.z - characters[j].transform.position.z;
+            if (!characters[j].isPlayer && !characters[j].isAttacking && newDir.magnitude<=characters[j].attackRange)
+                characters[j].Attack();
             newDir.Normalize();
-            character.moveDirection = newDir;
+            characters[j].moveDirection = newDir;
         }
     }
 
